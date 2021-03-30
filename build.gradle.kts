6a3feb118c1b6005cc2ea71ef94de99105bbbe8a
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2021-03-30 12:58:41 +0200

    added coroutines dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,5 +18,7 @@ aurora {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.4.2")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.2")
 }
