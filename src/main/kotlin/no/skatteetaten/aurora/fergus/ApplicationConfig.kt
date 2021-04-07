00e60e785b3c596987bb19367806588b16085709
Engebretsen, Tomas <Tomas.Engebretsen@skatteetaten.no>
2021-04-07 08:48:22 +0200

    Fikser merge-feil og legger til ApiClient-bønne

--- src/main/kotlin/no/skatteetaten/aurora/fergus/ApplicationConfig.kt
+++ src/main/kotlin/no/skatteetaten/aurora/fergus/ApplicationConfig.kt
@@ -1,20 +1,9 @@
 package no.skatteetaten.aurora.fergus
 
-import io.netty.channel.ChannelOption
-import io.netty.handler.ssl.SslContextBuilder
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory
-import io.netty.handler.timeout.ReadTimeoutHandler
-import io.netty.handler.timeout.WriteTimeoutHandler
-import mu.KotlinLogging
-import no.skatteetaten.aurora.fergus.integration.HEADER_AURORA_TOKEN
-import no.skatteetaten.aurora.fergus.security.SharedSecretReader
 import no.skatteetaten.aurora.fergus.service.StorageGridServiceReactive
 import org.openapitools.client.ApiClient
 import org.openapitools.client.api.AuthApi
 import org.openapitools.client.api.S3Api
-import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -22,7 +11,10 @@ import org.springframework.context.annotation.Configuration
 class ApplicationConfig {
 
     @Bean
-    fun storageGridService(webClient: WebClient) = StorageGridServiceReactive(webClient)
+    fun apiClient() = ApiClient()
+
+    @Bean
+    fun storageGridService(authApi: AuthApi) = StorageGridServiceReactive(authApi)
 
     @Bean
     fun storageGridS3Api(storageGridApiClient: ApiClient): S3Api = S3Api(storageGridApiClient)
