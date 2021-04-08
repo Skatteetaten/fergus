298d2ddeec06e8ec69e1dc56984e1f8590c64e1b
Tomas Engebretsen <tomas.engebretsen@gmail.com>
2021-04-08 08:15:31 +0200

    AOS-5595 Add Swagger codegen for Storagegrid client (#1)
    * AOS-5595 Add Swagger codegen for Storagegrid client
    
    * initial commit
    
    * example on cleaner config, with sunshine test
    
    * Fikser merge-feil og legger til ApiClient-bønne
    
    * Remove unnecessary things
    
    * remove requirement annotation
    
    Co-authored-by: Anders Mikkelsen <Anders.Mikkelsen@skatteetaten.no>

--- src/main/kotlin/no/skatteetaten/aurora/fergus/service/StorageGridService.kt
+++ src/main/kotlin/no/skatteetaten/aurora/fergus/service/StorageGridService.kt
@@ -1,20 +1,20 @@
 package no.skatteetaten.aurora.fergus.service
 
-import no.skatteetaten.aurora.fergus.controllers.AuthorizationPayload
-import org.springframework.web.reactive.function.client.WebClient
+import kotlinx.coroutines.reactive.awaitSingle
 import no.skatteetaten.aurora.fergus.FergusException
-import no.skatteetaten.aurora.fergus.ServiceTypes
-import no.skatteetaten.aurora.fergus.TargetService
+import no.skatteetaten.aurora.fergus.controllers.AuthorizationPayload
+import org.openapitools.client.api.AuthApi
+import org.openapitools.client.model.AuthorizeResponse
+import org.openapitools.client.model.Credentials
+import org.springframework.stereotype.Service
 
-class StorageGridServiceReactive(
-    @TargetService(ServiceTypes.STORAGEGRID) private val webClient: WebClient
-) : StorageGridService {
-    override suspend fun authorize(authorizationPayload: AuthorizationPayload): AuthorizeResponse? = webClient
-        .post()
-        .uri("/api/v3/authorize")
-        .authorize()
-
-    private suspend inline fun WebClient.RequestHeadersSpec<*>.authorize(): AuthorizeResponse? = null
+@Service
+class StorageGridServiceReactive(private val storageGridAuthApi: AuthApi) : StorageGridService {
+    override suspend fun authorize(
+        authorizationPayload: AuthorizationPayload
+    ): AuthorizeResponse = storageGridAuthApi
+        .authorizePost(authorizationPayload.toAuthorizeInput())
+        .awaitSingle()
 }
 
 interface StorageGridService {
@@ -24,21 +24,7 @@ interface StorageGridService {
         throw FergusException("StorageGrid integration is disabled for this environment")
 }
 
-data class AuthorizeInput(
-    val accountId: String,
-    val username: String,
-    val password: String,
-)
-
-fun AuthorizationPayload.toAuthorizeInput() = AuthorizeInput(
-    accountId = accountId,
-    username = username,
-    password = password,
-)
-
-data class AuthorizeResponse(
-    val status: String,
-    val apiVersion: String,
-    val deprecated: Boolean?,
-    val data: String,
-)
+fun AuthorizationPayload.toAuthorizeInput(): Credentials = Credentials()
+    .accountId(accountId)
+    .username(username)
+    .password(password)
---
+++ src/test/kotlin/no/skatteetaten/aurora/fergus/service/StorageGridServiceTest.kt
@@ -0,0 +1,75 @@
+package no.skatteetaten.aurora.fergus.service
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.runBlocking
+import no.skatteetaten.aurora.fergus.controllers.AuthorizationPayload
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.openapitools.client.model.AuthorizeResponse
+import org.openapitools.client.model.AuthorizeResponse.StatusEnum.SUCCESS
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpHeaders.AUTHORIZATION
+import org.springframework.http.HttpHeaders.CONTENT_TYPE
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+
+@SpringBootTest(
+    properties = [
+        "integrations.storagegrid.url=http://localhost:9999",
+        "aurora.token.value=testToken",
+    ]
+)
+class StorageGridServiceTest {
+    @Autowired
+    lateinit var storageGridService: StorageGridService
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
+
+    private lateinit var mockWebServer: MockWebServer
+
+    @BeforeEach
+    fun setup() {
+        mockWebServer = MockWebServer()
+        mockWebServer.start(9999)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun `sunshine test`() {
+        mockWebServer.enqueue(
+            MockResponse()
+                .setResponseCode(200)
+                .setHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .setBody(
+                    objectMapper.writeValueAsString(
+                        AuthorizeResponse().apply {
+                            status = SUCCESS
+                        }
+                    )
+                )
+        )
+
+        runBlocking {
+            val body = AuthorizationPayload(
+                accountId = "testAccount",
+                username = "testUser",
+                password = "testPass",
+            )
+            val token = storageGridService.authorize(body)
+            val request = mockWebServer.takeRequest()
+
+            assertThat(token?.status).isEqualTo(SUCCESS)
+            assertThat(request.getHeader(AUTHORIZATION)).isEqualTo("aurora-token testToken")
+            assertThat(request.path).isEqualTo("/api/v3/authorize")
+            assertThat(request.body.readUtf8()).isEqualTo(objectMapper.writeValueAsString(body.toAuthorizeInput()))
+        }
+    }
+}
