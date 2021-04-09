6940eb82a193b4f26d276d10afec9840ff9b3924
Rune Offerdal <Rune.Offerdal@skatteetaten.no>
2021-04-09 16:08:29 +0200

    AOS-5594 Temporary store (NB does not build)

--- src/main/kotlin/no/skatteetaten/aurora/fergus/ApplicationConfig.kt
+++ src/main/kotlin/no/skatteetaten/aurora/fergus/ApplicationConfig.kt
@@ -2,6 +2,7 @@ package no.skatteetaten.aurora.fergus
 
 import org.openapitools.client.ApiClient
 import org.openapitools.client.api.AuthApi
+import org.openapitools.client.api.ContainersApi
 import org.openapitools.client.api.S3Api
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,4 +14,7 @@ class ApplicationConfig {
 
     @Bean
     fun storageGridAuthApi(storageGridApiClient: ApiClient): AuthApi = AuthApi(storageGridApiClient)
+
+    @Bean
+    fun storageGridContainersApi(storageGridApiClient: ApiClient): ContainersApi = ContainersApi(storageGridApiClient)
 }
--- src/main/kotlin/no/skatteetaten/aurora/fergus/service/StorageGridService.kt
+++ src/main/kotlin/no/skatteetaten/aurora/fergus/service/StorageGridService.kt
@@ -4,14 +4,19 @@ import kotlinx.coroutines.reactive.awaitSingle
 import no.skatteetaten.aurora.fergus.FergusException
 import no.skatteetaten.aurora.fergus.controllers.AuthorizationPayload
 import org.openapitools.client.api.AuthApi
+import org.openapitools.client.api.ContainersApi
 import org.openapitools.client.model.AuthorizeResponse
+import org.openapitools.client.model.ContainerListResponse
 import org.openapitools.client.model.Credentials
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
 
 @Service
-class StorageGridServiceReactive(private val storageGridAuthApi: AuthApi) : StorageGridService {
+class StorageGridServiceReactive(
+    private val storageGridAuthApi: AuthApi,
+    private val storageGridContainersApi: ContainersApi,
+) : StorageGridService {
     override suspend fun authorize(
         authorizationPayload: AuthorizationPayload
     ): String {
@@ -26,10 +31,31 @@ class StorageGridServiceReactive(private val storageGridAuthApi: AuthApi) : Stor
         }
         return response.data // Returns authorization token
     }
+
+    override suspend fun provideBucket(
+        bucketName: String,
+        token: String
+    ): String {
+        storageGridContainersApi.apiClient.setBearerToken(token)
+        val bucketListResponse: ContainerListResponse = storageGridContainersApi
+            .orgContainersGet(listOf<String>())
+            .awaitSingle()
+        if (bucketListResponse.status === ContainerListResponse.StatusEnum.ERROR){
+            throw ResponseStatusException(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "The Storagegrid containers api returned an error"
+            )
+        }
+        // TODO: Check if bucketName exists in bucketListResponse, if not, create
+
+        return response.data // Returns bucket name
+    }
 }
 
 interface StorageGridService {
     suspend fun authorize(authorizationPayload: AuthorizationPayload): String = integrationDisabled()
+
+    suspend fun provideBucket(bucketName: String): String = integrationDisabled()
 
     private fun integrationDisabled(): Nothing =
         throw FergusException("StorageGrid integration is disabled for this environment")
