587f0eecc676c6c6b5f76aab7792b114863a075a
Rune Offerdal <Rune.Offerdal@skatteetaten.no>
2021-04-30 10:57:29 +0200

    AOS-5594 Testing of errorhandler started

--- src/test/kotlin/no/skatteetaten/aurora/fergus/error/ErrorHandlerTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/fergus/error/ErrorHandlerTest.kt
@@ -1,66 +1,67 @@
-package no.skatteetaten.aurora.fergus.controller
+package no.skatteetaten.aurora.fergus.error
 
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.coEvery
 import no.skatteetaten.aurora.fergus.controllers.UserPoliciesController
-import no.skatteetaten.aurora.fergus.service.S3AccessKeys
 import no.skatteetaten.aurora.fergus.service.StorageGridService
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.context.annotation.Import
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ReactiveHttpOutputMessage
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
 import org.springframework.web.reactive.function.BodyInserter
 import org.springframework.web.reactive.function.BodyInserters
-import java.util.UUID
+import org.springframework.web.server.ResponseStatusException
 
 @WebFluxTest(UserPoliciesController::class)
 @Import(WebClientAutoConfiguration::class)
-class UserPoliciesControllerTest {
-
+class ErrorHandlerTest {
     @MockkBean
     private lateinit var storageGridService: StorageGridService
     @Autowired
     lateinit var webTestClient: WebTestClient
 
     @Test
-    fun bucketsPathsUserpoliciesHappyTest() {
+    fun `Testing that ErrorHandler catches ResponseStatusException`() {
+        val responseStatusException = ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "The reason it occurred")
         coEvery {
             storageGridService.authorize(any())
-        } returns "testtoken"
-        coEvery {
-            storageGridService.provideBucket("bucket-1", "testtoken")
-        } returns "bucket-1"
-        coEvery {
-            storageGridService.provideGroup("bucket-1", "path-test", any(), "testtoken")
-        } returns UUID.fromString("847b3329-9855-4da9-bf3b-b2162e993d5d")
-        coEvery {
-            storageGridService.provideUser("username", UUID.fromString("847b3329-9855-4da9-bf3b-b2162e993d5d"), "testtoken")
-        } returns UUID.fromString("7b579332-87c5-4dc2-9ba0-dfe9dda435c3")
-        coEvery {
-            storageGridService.assignPasswordToUser(UUID.fromString("7b579332-87c5-4dc2-9ba0-dfe9dda435c3"), "passord", "testtoken")
-        } returns "passord"
-        coEvery {
-            storageGridService.provideS3AccessKeys(UUID.fromString("7b579332-87c5-4dc2-9ba0-dfe9dda435c3"), "testtoken")
-        } returns S3AccessKeys("s3AccessKey", "s3SecretKey")
+        } throws (responseStatusException)
 
         webTestClient
             .post()
             .uri("/v1/buckets/bucket-1/paths/path-test/userpolicies/")
             .contentType(MediaType.APPLICATION_JSON)
-            .body(createBody())
+            .body(createUserPoliciesBody())
             .exchange()
-            .expectStatus().isOk()
-            .expectHeader().contentType(MediaType.APPLICATION_JSON)
+            .expectStatus().is5xxServerError()
             .expectBody()
-            .json("{\"username\":\"username\",\"password\":\"passord\",\"host\":\"http://uia0ins-netapp-storagegrid01.skead.no:10880/\",\"s3accesskey\":\"s3AccessKey\",\"s3secretaccesskey\":\"s3SecretKey\"}")
+            .returnResult().toString().contains("The reason it occurred")
     }
 
-    private fun createBody(): BodyInserter<String, ReactiveHttpOutputMessage> = BodyInserters.fromValue(
+    private fun createUserPoliciesBody(): BodyInserter<String, ReactiveHttpOutputMessage> = BodyInserters.fromValue(
         """{"tenantAccount":{"accountId":"accountId","username":"tausername","password":"tapassword"}, "username":"username", "password":"passord", "access":["READ"]}"""
     )
+
+    // TODO: @Test
+    fun `Testing that ErrorHandler catches generic exception`() {
+        val exception = Exception("The reason it occurred")
+        coEvery {
+            storageGridService.authorize(any())
+        } throws (exception)
+
+        webTestClient
+            .post()
+            .uri("/v1/authorize/")
+            .contentType(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().is5xxServerError()
+            .expectBody()
+            .returnResult().toString().contains("The reason it occurred")
+    }
 }
