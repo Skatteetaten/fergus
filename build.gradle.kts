69c4de8fbd6532d96d10cd7c8a9b67116f6bc80f
Rune Offerdal <Rune.Offerdal@skatteetaten.no>
2022-03-09 08:44:59 +0100

    AOS-6462 bump aurora gradle plugin to 4.4.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("idea")
     id("java")
     id("org.openapi.generator") version "5.2.0"
-    id("no.skatteetaten.gradle.aurora") version("4.3.13")
+    id("no.skatteetaten.gradle.aurora") version("4.4.12")
 }
 
 aurora {
