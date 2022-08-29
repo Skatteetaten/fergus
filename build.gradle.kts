fbefb0a1502b94ea7f7f03463be1c6a965c5bbf9
Rune Offerdal <52204930+RuneOfferdal@users.noreply.github.com>
2022-08-29 08:25:25 +0200

    AOS-6811 Bump dependencies (#16)
    * AOS-6811 Bump dependencies
    
    * AOS-6811 removing ref to mavenCentral
    
    * AOS-6811 bumping gradle wrapper version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("idea")
     id("java")
     id("org.openapi.generator") version "5.2.0"
-    id("no.skatteetaten.gradle.aurora") version("4.4.22")
+    id("no.skatteetaten.gradle.aurora") version("4.5.4")
 }
 
 aurora {
@@ -17,12 +17,12 @@ aurora {
 
 dependencies {
     implementation("javax.validation:validation-api:2.0.1.Final")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.1")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.7")
 
     // Swagger
-    implementation("org.openapitools:jackson-databind-nullable:0.2.2")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.3")
     implementation("io.swagger:swagger-annotations:1.6.6")
     implementation("com.google.code.findbugs:jsr305:3.0.2")
 
